8922bc2efd0857f928a44144911ce29ec74e8d9c
girikaushik <kaushik.gla_cs19@gla.ac.in>
2023-08-01 21:14:37 +0530

    movie-info service

---
+++ movie-info-service/src/main/java/com/javabrains/movieinfoservice/MovieResource.java
@@ -0,0 +1,17 @@
+package com.javabrains.movieinfoservice;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.javabrains.movieinfoservice.models.Movie;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieResource {
+
+	@RequestMapping("/{movieId}")
+	public Movie getMovieInfo(@PathVariable("movieId") String movieId){
+		return new Movie(movieId,"name"); 
+	} 
+}
